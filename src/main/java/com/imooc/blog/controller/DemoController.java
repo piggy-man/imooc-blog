32c713a0c0d3856181972d48e43df954fbb8f5ff
Gavin <1146855407@qq.com>
2018-10-12 00:04:06 +0800

    1-1 第一次使用gradle创建项目框架，修改中央仓库地址到阿里的中央仓库。

--- src/main/java/com/imooc/blog/controller/DemoController.java
+++ src/main/java/com/imooc/blog/controller/DemoController.java
@@ -1,4 +1,12 @@
 package com.imooc.blog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello spring-boot!";
+    }
 }
